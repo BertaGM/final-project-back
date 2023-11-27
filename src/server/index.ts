73d4f3874b94b4f6f13b4629696b8beadfb12449
Berta González <bertagonzalezmartin@gmail.com>
2023-11-27 19:50:33 +0100

    Add morgan, express and CORS controll

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,1 +1,12 @@
-// La cadena de middlewares 📎
+import morgan from "morgan";
+import express from "express";
+import cors from "cors";
+import app from "./app.js";
+
+const corsPort = process.env.ALLOWED_ORIGIN;
+const corsOption = { origin: corsPort };
+
+app.use(cors(corsOption));
+
+app.use(morgan("dev"));
+app.use(express.json());
