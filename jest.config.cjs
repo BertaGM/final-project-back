97cc80433e79ea022e69ab93c3930d66cf66f13c
Berta González <bertagonzalezmartin@gmail.com>
2023-11-30 16:40:22 +0100

    Refactor jest config file

--- jest.config.cjs
+++ jest.config.cjs
@@ -4,7 +4,6 @@ module.exports = {
   testEnvironment: "node",
   testMatch: ["**/src/**/*.test.ts"],
   resolver: "jest-ts-webcompat-resolver",
-  setupFilesAfterEnv: ["./src/setupTests.ts"],
   coveragePathIgnorePatterns: [
     "src/index.ts, src/server/app.ts, src/database/index.ts",
   ],
