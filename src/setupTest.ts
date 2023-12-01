43b5d02f5887c9ae2e78410175b603473e717407
Berta González <bertagonzalezmartin@gmail.com>
2023-12-01 12:22:45 +0100

    Fix coverage rules in setupTest and jest config files

--- src/setupTest.ts
+++ src/setupTest.ts
@@ -6,7 +6,16 @@ import "./server/index";
 
 let server: MongoMemoryServer;
 
+const serverConnection = async () => {
+  try {
+    server = await MongoMemoryServer.create();
+  } catch {
+    await MongoMemoryServer.create();
+  }
+};
+
 beforeAll(async () => {
+  await serverConnection();
   server = await MongoMemoryServer.create();
   const mongoUrl = server.getUri();
   await connectToDatabase(mongoUrl);
