1447c0e01f5750a5c30cd4b4c0c47c886bdb2588
Berta González <bertagonzalezmartin@gmail.com>
2023-12-07 11:54:36 +0100

    Add create controller method

--- src/features/balls/controller/BallsController.ts
+++ src/features/balls/controller/BallsController.ts
@@ -1,12 +1,7 @@
 import type { Request, Response, NextFunction } from "express";
 import type { BallsRepository } from "../repository/types";
 import CustomError from "../../../server/CustomError/CustomError.js";
-
-export type UpdateBallRequest = Request<
-  Record<string, unknown>,
-  Record<string, unknown>,
-  { _id: string; isTengui: boolean }
->;
+import { type UpdateBallRequest, type BallRequestWithoutId } from "../types";
 
 class BallsController {
   constructor(private readonly ballsRepository: BallsRepository) {}
@@ -23,6 +18,7 @@ class BallsController {
     next: NextFunction,
   ): Promise<void> => {
     const { ballId } = req.params;
+
     try {
       await this.ballsRepository.deleteBall(ballId);
 
@@ -33,14 +29,34 @@ class BallsController {
     }
   };
 
+  public addBall = async (
+    req: BallRequestWithoutId,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> => {
+    try {
+      const ball = req.body;
+
+      const ballWithId = await this.ballsRepository.addBall(ball);
+
+      res.status(201).json({ ball: ballWithId });
+    } catch {
+      const customError = new CustomError("Error adding a new ball", 400);
+
+      next(customError);
+    }
+  };
+
   public modifyIsTengui = async (
     req: UpdateBallRequest,
     res: Response,
     next: NextFunction,
   ): Promise<void> => {
     const { _id, isTengui } = req.body;
+
     try {
       const response = await this.ballsRepository.modifyIsTengui(_id, isTengui);
+
       res.status(200).json({ response });
     } catch (error) {
       const customError = new CustomError(
@@ -48,6 +64,7 @@ class BallsController {
         400,
         (error as Error).message,
       );
+
       next(customError);
     }
   };
