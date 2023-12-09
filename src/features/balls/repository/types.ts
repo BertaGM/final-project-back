dca64a5a9258e74e0e221137614c70d9a90fd619
Berta González <bertagonzalezmartin@gmail.com>
2023-12-09 18:52:43 +0100

    Add getBallById method at repository

--- src/features/balls/repository/types.ts
+++ src/features/balls/repository/types.ts
@@ -3,9 +3,10 @@ import type { BallStructureWithoutId, BallsStructure } from "../types";
 export interface BallsRepository {
   getBalls: () => Promise<BallsStructure[]>;
   deleteBall: (ballId: string) => Promise<void>;
+  addBall: (ball: BallStructureWithoutId) => Promise<BallsStructure>;
+  getBallById: (_id: string) => Promise<BallsStructure>;
   modifyIsTengui: (
     ballId: string,
     ballIsTengui: boolean,
   ) => Promise<void | BallsStructure>;
-  addBall: (ball: BallStructureWithoutId) => Promise<BallsStructure>;
 }
